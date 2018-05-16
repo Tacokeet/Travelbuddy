a8bd45ee002c60a5010b3003e8f8a4c0fa2846e7
Jelle de Vries <jelle_de_vriesie@hotmail.com>
2018-05-16 11:38:20 +0200

    Lots of changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import './App.css';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import Main from './main/Main';
+import Login from './main/user/Login';
+import Profile from './main/user/Profile';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+
+import { 
+	Route,
+	BrowserRouter
+} from 'react-router-dom';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			
+		}
+	}
+	
   render() {
     return (
-      <div className="App">
+		<BrowserRouter>
+		
+			<div className="App">
 	  
-		<Header />
-		
-		<Main />
-		
-		<Footer />
-		
-      </div>
+				<Header />
+				
+				<Route exact path="/" component={Main} />
+				<Route path="/login" component={Login} />
+				<Route path="/profile" component={Profile} />
+				
+				<Footer />
+			
+			</div>
+			
+		</BrowserRouter>
     );
   }
+
 }
 
 export default App;
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { 
+	NavLink,
+} from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faCoffee from '@fortawesome/fontawesome-free-regular/faUser'
 
 class Header extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			showDropDown: false
+		}
+		
+		this.toggleMenu = this.toggleMenu.bind(this);
+	}
+	
 	render() {
 		return (
 			<header>
 				<h1>Welcome to TravelBuddy</h1>
+				<div id="dropDown">
+					<div id="dropDownIcon" onClick={this.toggleMenu}>
+						<FontAwesomeIcon icon={faCoffee} />
+					</div>
+					{this.state.showDropDown ? <DropDown /> : null}
+				</div>
 			</header>
 		);
+	}
+	
+	toggleMenu() {
+		this.setState({
+			showDropDown: !this.state.showDropDown
+		})
+	}
+}
+
+class DropDown extends Component {
+	render() {
+		return (
+			<ul id="menu">
+				<li><NavLink to="/login">Login</NavLink></li>
+				<li><NavLink to="/">Home</NavLink></li>
+			</ul>
+		)
 	}
 }
 
