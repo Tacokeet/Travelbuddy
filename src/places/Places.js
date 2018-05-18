ab38311fb213e3b904aae8146f3fbb4bfa6784e6
Tacokeet <soldnor@live.nl>
2018-05-18 10:00:23 +0200

    Commiting for pull sake

--- src/places/Places.js
+++ src/places/Places.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Places.css';
+import logo from "../images/6.JPG"
 
 const places = (props) => {
-    return (
-    <div>
-        <h3 className={'placesText'} >{props.categories}</h3>
-        <div className={'places'}></div>
+    return <div>
+        <h3 className={'placesText'}>{props.categories}</h3>
+        <div className={'places'}>
+            <img src={logo} alt="favicon"/>
+        </div>
 
     </div>
-    )
 
 }
 
