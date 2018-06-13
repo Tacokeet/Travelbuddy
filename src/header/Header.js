e8f134fcc6a460cd7e6a235f9f83158707150b48
Wouter Kurvers <WKurvers11@gmail.com>
2018-06-13 11:26:21 +0200

    Merge branch 'master' of https://github.com/wkurvers/Travelbuddy
    # Conflicts:
    #	src/App.js

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,8 +8,26 @@ import userIcon from '@fortawesome/fontawesome-free-regular/faUser'
 import searchIcon from '@fortawesome/fontawesome-free-solid/faSearch'
 import './Header.css';
 import mainLogo from '../images/logo.jpg';
+import axios from "axios/index";
 
 class Header extends Component {
+
+    state = {
+        name: "not logged in"
+    }
+
+    constructor(props) {
+        super(props);
+
+        const url = "/api/loginCheck"
+
+        axios.get(url)
+            .then(response => {
+                this.setState({name: response.data.username})
+            });
+    }
+
+
 	render() {
 		return (
 			<header>
@@ -17,9 +35,10 @@ class Header extends Component {
 				<img className={'mainLogo'} src={mainLogo} alt="mainLogo" />
 				<h1 id={'headerText'}>TravelBuddy</h1>
                 </NavLink>
-				<DropDown />
 
+				<DropDown />
                 <div id="search">
+                    <h5 id="headerName" >{this.state.name}</h5>
                     <div id="searchIcon">
 						<NavLink to="/search"><FontAwesomeIcon icon={searchIcon} /></NavLink>
                     </div>
@@ -29,19 +48,23 @@ class Header extends Component {
 	}
 }
 
+
+
 class DropDown extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			showDropDown: false
+			showDropDown: false,
 		}
 		
 		this.toggleMenu = this.toggleMenu.bind(this);
 	}
+
 	
 	render() {
 		return (
+
 			<div id="dropDown">
 				<div id="dropDownIcon" onClick={this.toggleMenu}>
 					<FontAwesomeIcon icon={userIcon} />
@@ -68,13 +91,33 @@ class DropDown extends Component {
 }
 
 class Menu extends Component {
+
+    state = {
+        check: false,
+		showLogin: true
+    }
+
+    constructor(props) {
+        super(props);
+
+        const url = "/api/loginValue"
+        axios.get(url)
+            .then(response => {
+                this.setState({check: response.data.value});
+                this.setState({showLogin: !response.data.value});
+            });
+
+    }
+
+
 	render() {
 		return (
 			<ul id="menu">
 				<a onClick={() => this.dispatchNewRoute('/')}><li>Home</li></a>
-				<a onClick={() => this.dispatchNewRoute('/login')}><li>Login</li></a>
 				<a onClick={() => this.dispatchNewRoute('/profile')}><li>Profile</li></a>
 				<a onClick={() => this.dispatchNewRoute('/addEvent')}><li>Add Event</li></a>
+                <a  style={{display: this.state.showLogin ? 'block' : 'none' }}onClick={() => this.dispatchNewRoute('/login')}><li>Login</li></a>
+				<a  style={{display: this.state.check ? 'block' : 'none' }} onClick={() => this.dispatchNewRoute('/logout')}><li>logout</li></a>
 				{/*<li><NavLink to="/">Home</NavLink></li>
 				<li><NavLink to="/login">Login</NavLink></li>
 				<li><NavLink to="/profile">Profile</NavLink></li>
